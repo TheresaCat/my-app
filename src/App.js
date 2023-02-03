91c01bba0534d09d5d7401d9f9a15217068951dc
TheresaCat <122808833+TheresaCat@users.noreply.github.com>
2023-02-03 11:31:15 +0100

    initialise component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-
 import './App.css';
+import Component from './Component';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Wilkommen zum Git Workshop</h1>
+        <p>Mit diesem Hands-On Beispiel wollen wir gemeinsam schauen, wie Version Control im Collaborativem Development mit Hilfe von Git/GitHub funktioniert.</p>
         
       </header>
+      <Component name="Theresa" /> 
       
     </div>
   );
---
+++ src/Component.js
@@ -0,0 +1,16 @@
+
+function Component(probs) {
+  return (
+    <div className="Component">
+      <header className="Component-header">
+        <h2>Name: {probs.name}</h2>
+
+       
+        
+      </header>
+      
+    </div>
+  );
+}
+
+export default Component;
